3d3aacd9b9b58e552bad51f365c304eb4b30b516
mgradvohl <marinagradvohl@gmail.com>
2021-02-13 20:03:02 -0300

    Adds app bar to home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:krepi_web/screens/home/components/app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    Size size = MediaQuery.of(context).size;
+    // this size provides us total height and width of screen
+    return Scaffold(
+        body: Container(
+      height: size.height,
+      // it will take full height
+      width: size.width,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('assets/images/bg.png'),
+        fit: BoxFit.cover,
+      )),
+      child: Column(
+        children: <Widget>[
+          CustomAppBar(),
+        ],
+      ),
+    )
     );
   }
 }
